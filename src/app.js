8c7384b027a3fea818003890352935a3f1815191
Robert Evans <robertevans@robertevansmb.com>
2018-11-25 14:17:46 -0500

    Complete additional routes challenge

--- src/app.js
+++ src/app.js
@@ -16,11 +16,25 @@ const AddExpensePage = () => (
     </div>
 );
 
+const EditExpensePage = () => (
+    <div>
+        This is from my edit expense component
+    </div>
+);
+
+const HelpPage = () => (
+    <div>
+        This is from my help component
+    </div>
+);
+
 const routes = (
     <BrowserRouter>
         <div>
             <Route path="/" component={ExpenseDashboardPage} exact={true}  />
             <Route path="/create" component={AddExpensePage} />
+            <Route path="/edit" component={EditExpensePage} />
+            <Route path="/help" component={HelpPage} />
         </div>
     </BrowserRouter>
 );
